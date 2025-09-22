e9ef4709652d3b5bc6929cf1b92acad1c9943bad
Myckey-Nikki <myckey8207@gmail.com>
2025-09-22 19:43:44 +0000

    new

--- C_BASICS/reversestring.c
+++ C_BASICS/reversestring.c
@@ -1,21 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-char arr[200];
-int b=0;
-while (arr[200]='\0')
-{
-    b++;
-}
+int main() {
+    char arr[200];
+    scanf("%s", arr);  // Read string into array
+    int b = 0;
 
-for (int i = 0;arr[i]!='\0'; i++)
-{
-    scanf("%s",&arr[i]);
-    printf("%s",arr[b-i-1]);
+    while (arr[b] != '\0') {
+        b++;
+    }
+    for (int i = 0;arr[i]!=0; i++)
+    {
+            
+printf("%c",arr[b-i-1]);
+    }
 
-}
-
-
-
+   
+    
     return 0;
 }
