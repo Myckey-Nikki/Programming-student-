71b64a1e31316429eaf9d981dd2570032aee255f
Myckey-Nikki <myckey8207@gmail.com>
2025-09-22 19:38:44 +0000

    git

---
+++ C_BASICS/reversestring.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+char arr[200];
+int b=0;
+while (arr[200]='\0')
+{
+    b++;
+}
+
+for (int i = 0;arr[i]!='\0'; i++)
+{
+    scanf("%s",&arr[i]);
+    printf("%s",arr[b-i-1]);
+
+}
+
+
+
+    return 0;
+}
